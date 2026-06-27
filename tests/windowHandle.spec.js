07a942b553a1b2f8ba05215639e121c9f5aa9e26
supriya775 <supriya.satyukt@gmail.com>
2026-06-27 09:11:03 +0530

    First commit

---
+++ tests/windowHandle.spec.js
@@ -0,0 +1,22 @@
+const { test, chromium } = require('@playwright/test');
+
+test('Window handling', async () => {
+    const browser = await chromium.launch({ headless: false });
+    const context = await browser.newContext();
+    const page = await context.newPage();
+
+    await page.goto('https://prafpawar11.github.io/multiplewindows.html');
+
+    await page.locator("//input[@name='fname']").fill('Ankit');
+
+    const [saucePage] = await Promise.all([
+        context.waitForEvent('page'),
+        page.locator("//a[text()='SauceDemo Link']").click()
+    ]);
+
+    await saucePage.waitForLoadState();
+    await saucePage.locator('#user-name').fill('standard_user');
+    await saucePage.locator('#password').fill('secret_sauce');
+    await saucePage.locator('#login-button').click();
+    
+});
